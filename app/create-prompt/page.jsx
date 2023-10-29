1fa55e6f38a8f970eff506ea92c56bfec2655d3f
Prajwal Mandlik <99119449+prajwalmandlik2004@users.noreply.github.com>
2023-10-29 19:44:07 +0530

    first commit

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
-const CreatePrompt = () => {
+  const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
